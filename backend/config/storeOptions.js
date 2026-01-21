5fdd29826493874ce991b419e91fed1cae7d835d
Amit More <156109795+amitmore-007@users.noreply.github.com>
2026-01-21 23:01:07 +0530

    Fix/contact tab (#18)
    * fix: added seed script to add missing stores in db
    
    * fix: formatting

---
+++ backend/config/storeOptions.js
@@ -0,0 +1,30 @@
+// Master store list for seeding/initialization.
+// Keep in sync with frontend/src/utils/storeOptions.js
+
+export const STORE_OPTIONS = [
+  "Magarpatta",
+  "Kharadi",
+  "Viman Nagar",
+  "Wagholi",
+  "Warje",
+  "Bavdhan",
+  "Bibwewadi",
+  "Koregaon Park",
+  "MG Road",
+  "Salunkhe Vihar",
+  "JM Road",
+  "Aundh",
+  "Pimple Saudagar",
+  "Balewadi",
+  "Chinchwad",
+  "Ravet",
+  "Wakad",
+  "Happiness Street",
+  "Kothrud",
+  "Sinhgad Road",
+  "Hinjewadi",
+  "Undri",
+  "Dhanori",
+];
+
+export default STORE_OPTIONS;
